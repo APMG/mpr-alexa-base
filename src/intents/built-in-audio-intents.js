31f79f1d8f48a27ab2a1a45f25fa4ce8f93431d9
Eileen Noonan <enoonan@mpr.org>
2017-11-03 11:13:20 -0500

    separate built in intents from audio intents

--- src/intents/built-in-audio-intents.js
+++ src/intents/built-in-audio-intents.js
@@ -11,25 +11,10 @@ exports.default = function (stationConfig) {
 }
 
 const builtIns = {
-  'LaunchRequest': function () {
-    directives.addPlayDirective(this, config.STREAM_URL)
-    this.response
-      .cardRenderer(config.CARD_TITLE, config.CARD_CONTENT)
-      .speak(config.SPOKEN_WELCOME || 'Welcome to ' + config.STATION_NAME)
-
-    this.emit(':responseReady')
-  },
-  'AMAZON.HelpIntent': function () {
-    this.emit(':tell', config.SPOKEN_HELP)
-  },
   'AMAZON.ResumeIntent': function () {
     addPlayDirective(this, config.STREAM_URL)
     this.emit(':responseReady')
   },
-  'Unhandled': function () {
-    this.emit(':tell', config.SPOKEN_UNHANDLED)
-  },
-  'AMAZON.CancelIntent': function () { stop(this) },
   'AMAZON.PauseIntent': function () { stop(this) },
   'AMAZON.StopIntent': function () { stop(this) },
   'AMAZON.NextIntent': function () { cannotDoForLiveStream(this) },
@@ -39,9 +24,6 @@ const builtIns = {
   'AMAZON.ShuffleOnIntent': function () { cannotDoForLiveStream(this) },
   'AMAZON.ShuffleOffIntent': function () { cannotDoForLiveStream(this) },
   'AMAZON.StartOverIntent': function () { cannotDoForLiveStream(this) },
-  'SessionEndedRequest': function () {
-    this.emit(':saveState', true)
-  },
   'PlaybackNearlyFinished': noop
 }
 
