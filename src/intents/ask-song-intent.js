59219226a5f906b693f82f60b3bd343f9f5679be
Eileen Noonan <enoonan@mpr.org>
2017-11-03 11:52:37 -0500

    hopefully fix ask song intent...

--- src/intents/ask-song-intent.js
+++ src/intents/ask-song-intent.js
@@ -4,53 +4,53 @@ var speechBuilder = require('../speech-builder')
 
 var config
 
-exports.default = function (stationConfig, handler, error) {
+exports.default = function (stationConfig) {
   config = stationConfig
 
   return {
     'AskSongIntent': function () {
       return got(config.NOW_PLAYING_URL + '/playlist')
         .then(
-          function handler (response) {
-            let body = JSON.parse(response.body)
-            let songs = get(body, 'data.songs', null)
-
-            if (!songs || !songs.length || !songs[0]) {
-              this.emit(':tell', config.SPOKEN_CANNOT_FIND)
-              return
-            }
-
-            const messageSpec = [
-              {
-                beforeText: 'the song',
-                key: 'title'
-              },
-              {
-                beforeText: 'by',
-                key: 'artist'
-              },
-              {
-                beforeText: 'from the album',
-                key: 'album'
-              }
-            ]
-            let songDesc = speechBuilder.buildMessageFromSpec(songs[0], messageSpec)
-
-            this.emit(
-              ':tellWithCard',
-              'Now playing ' + songDesc,
-              'Now Playing',
-              songDesc
-            )
-          },
-          error || defaultError().bind(this)
+          handler.bind(this),
+          defaultError.bind(this)
         )
     }
   }
 }
 
-function defaultError () {
-  return function (error) {
-    this.emit(':tell', config.SPOKEN_ERROR)
+const handler = function (response) {
+  let body = JSON.parse(response.body)
+  let songs = get(body, 'data.songs', null)
+
+  if (!songs || !songs.length || !songs[0]) {
+    this.emit(':tell', config.SPOKEN_CANNOT_FIND)
+    return
   }
+
+  const messageSpec = [
+    {
+      beforeText: 'the song',
+      key: 'title'
+    },
+    {
+      beforeText: 'by',
+      key: 'artist'
+    },
+    {
+      beforeText: 'from the album',
+      key: 'album'
+    }
+  ]
+  let songDesc = speechBuilder.buildMessageFromSpec(songs[0], messageSpec)
+
+  this.emit(
+    ':tellWithCard',
+    'Now playing ' + songDesc,
+    'Now Playing',
+    songDesc
+  )
+}
+
+const defaultError = function (error) {
+  this.emit(':tell', config.SPOKEN_ERROR)
 }
