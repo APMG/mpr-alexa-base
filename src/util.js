2a8d14b241204d004b9c9433e911f103eebc6368
Eileen Noonan <enoonan@mpr.org>
2017-11-07 16:18:13 -0600

    fix phoneme

--- src/util.js
+++ src/util.js
@@ -7,17 +7,21 @@ exports.arrayToSentence = function (arr) {
 }
 
 exports.replacePhonemes = function (string, phonemeDictionary) {
+  console.log('replacing phonemes for ' + string)
+  console.log(phonemeDictionary)
   for (var phoneme in phonemeDictionary) {
     if (string.indexOf(phoneme) !== -1) {
+      console.log(phoneme)
+      console.log(phonemeDictionary[phoneme])
       string = string.replace(
         phoneme,
-        '<phoneme alphabet="ipa"' +
-          'ph="' + phonemeDictionary[phoneme] + '">' +
+        "<phoneme alphabet='ipa' " +
+          "ph='" + phonemeDictionary[phoneme] + "'>" +
           phoneme +
         '</phoneme>'
       )
     }
   }
-
+  console.log(string)
   return string
 }
